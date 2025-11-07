6e589223c88612cd8da2440acbf0019a9ee94b99
Tanner Carter <641339+tanner-c@users.noreply.github.com>
2025-11-07 18:04:22 -0500

    Remove unused imports from main

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,4 @@
-import * as THREE from 'three';
 import { Engine } from './engine';
-import { Component } from './component';
-import { FreeCamComponent } from './camera';
 import { initializeGame } from './example-game';
 
 const engine = new Engine({
