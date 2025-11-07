aea40e1143a90374993966d67cc179e72ab837b9
Tanner Carter <641339+tanner-c@users.noreply.github.com>
2025-11-07 17:50:20 -0500

    Move game code into separate file

---
+++ src/example-game.ts
@@ -0,0 +1,80 @@
+import { Component } from './component';
+import { Engine } from './engine';
+import * as THREE from 'three/webgpu';
+
+class PlayerComponent extends Component {
+    constructor(object3D: THREE.Object3D) {
+        super(object3D);
+
+        this.bindAction({
+            name: 'moveForward',
+            type: 'keyboard',
+            code: 'KeyW',
+            callback: (value: number) => {
+                this.object3D.position.z -= value * 0.1;
+            }
+        });
+
+        this.bindAction({
+            name: 'moveBackward',
+            type: 'keyboard',
+            code: 'KeyS',
+            callback: (value: number) => {
+                this.object3D.position.z += value * 0.1;
+            }
+        });
+
+
+        this.bindAction({
+            name: 'moveLeft',
+            type: 'keyboard',
+            code: 'KeyA',
+            callback: (value: number) => {
+                this.object3D.position.x -= value * 0.1;
+            }
+        });
+
+        this.bindAction({
+            name: 'moveRight',
+            type: 'keyboard',
+            code: 'KeyD',
+            callback: (value: number) => {
+                this.object3D.position.x += value * 0.1;
+            }
+        });
+    }
+}
+
+export function initializeGame(engine: Engine) {
+    const { scene, camera } = engine;
+
+    // Create floor
+    const floorGeometry = new THREE.BoxGeometry(10, 1, 10);
+    const floorMaterial = new THREE.MeshPhysicalMaterial({
+        color: '#df1010ff',
+        roughness: 0.5,
+        metalness: 0.5,
+    });
+
+    const floorMesh = new THREE.Mesh(floorGeometry, floorMaterial);
+    floorMesh.position.y = -0.5;
+    scene.add(floorMesh);
+
+    // Create empty player object
+
+    const player = new THREE.Object3D();
+    player.add(camera);
+    camera.position.set(0, 1.6, 0);
+
+    player.position.y = 1;
+    scene.add(player);
+
+    new PlayerComponent(player);
+
+
+    // Initialize light
+    const light = new THREE.DirectionalLight(0xffffff, 1);
+    light.position.set(5, 10, 7.5);
+    scene.add(light);
+
+}
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import * as THREE from 'three';
 import { Engine } from './engine';
 import { Component } from './component';
 import { FreeCamComponent } from './camera';
+import { initializeGame } from './example-game';
 
 const engine = new Engine({
   rendererParameters: {
@@ -9,43 +10,4 @@ const engine = new Engine({
   }
 });
 
-const { scene } = engine;
-
-// Create floor
-const floor = new THREE.BoxGeometry(10, 1, 10);
-const material = new THREE.MeshPhysicalMaterial({
-  color: '#df1010ff',
-  roughness: 0.5,
-  metalness: 0.5,
-});
-
-const floorMesh = new THREE.Mesh(floor, material);
-floorMesh.position.y = -0.5;
-
-const cubeGeometry = new THREE.BoxGeometry();
-const cubeMaterial = new THREE.MeshPhysicalMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
-cube.position.y = 1;
-
-const cubeRotateComponent = new Component(cube, () => {
-  cubeRotateComponent.object3D.rotation.x += 0.01;
-});
-
-// Initialize light
-const light = new THREE.DirectionalLight(0xffffff, 1);
-light.position.set(5, 10, 7.5);
-
-const dayCycleComponent = new Component(light, () => {
-  const time = Date.now() * 0.0005;
-  light.position.x = Math.sin(time) * 10;
-  light.position.z = Math.cos(time) * 10;
-  light.position.y = Math.sin(time * 2) * 5 + 5;
-});
-
-const freeCam = new FreeCamComponent(engine.camera);
-
-engine.camera.position.set(0, 2, 5);
-
-scene.add(floorMesh);
-scene.add(light);
-scene.add(cube);
+initializeGame(engine);
